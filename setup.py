157b0652a63aff6289497f548fb069485d025c8b
James Hiebert <hiebert@uvic.ca>
2014-01-22 14:22:35 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '0.2'
+version = '0.4'
 
 install_requires = [
     'pupynere >=1.1.2a2',
