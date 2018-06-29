90134a01f5980ecbfb85096734db05fc00765da6
James Hiebert <hiebert@uvic.ca>
2018-06-29 15:07:24 -0700

    Bump version to 0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '0.5.1'
+version = '0.5.2'
 
 install_requires = [
     'numpy',
